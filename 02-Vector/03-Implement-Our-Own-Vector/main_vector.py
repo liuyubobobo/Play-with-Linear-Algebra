aee455af21d3ee088caf8a91fd57db14e6c351b6
liuyubobobo <liuyubobobo@gmail.com>
2018-07-20 14:43:50 -0700

    Chapter 02 folder added.

---
+++ 02-Vector/03-Implement-Our-Own-Vector/main_vector.py
@@ -0,0 +1,18 @@
+from playLA.Vector import Vector
+
+if __name__ == "__main__":
+
+    vec = Vector([5, 2])
+    print(vec)
+    print("len(vec) = {}".format(len(vec)))
+    print("vec[0] = {}, vec[1] = {}".format(vec[0], vec[1]))
+
+    vec2 = Vector([3, 1])
+    print("{} + {} = {}".format(vec, vec2, vec + vec2))
+    print("{} - {} = {}".format(vec, vec2, vec - vec2))
+
+    print("{} * {} = {}".format(vec, 3, vec * 3))
+    print("{} * {} = {}".format(3, vec, 3 * vec))
+
+    print("+{} = {}".format(vec, +vec))
+    print("-{} = {}".format(vec, -vec))
---
+++ 02-Vector/03-Implement-Our-Own-Vector/playLA/Vector.py
@@ -0,0 +1,46 @@
+class Vector:
+
+    def __init__(self, lst):
+        self._values = lst;
+
+    def __add__(self, another):
+        """向量加法，返回结果向量"""
+        assert len(self) == len(another), \
+            "Error in adding. Length of vectors must be same."
+
+        # return Vector([a + b for a, b in zip(self._values, another._values)])
+        return Vector([a + b for a, b in zip(self, another)])
+
+    def __sub__(self, another):
+        """向量加法，返回结果向量"""
+        assert len(self) == len(another), \
+            "Error in subtracting. Length of vectors must be same."
+
+        return Vector([a - b for a, b in zip(self, another)])
+
+    def __mul__(self, k):
+        return Vector([k * e for e in self])
+
+    def __rmul__(self, k):
+        return self * k;
+
+    def __pos__(self):
+        return 1 * self
+
+    def __neg__(self):
+        return -1 * self
+
+    def __iter__(self):
+        return self._values.__iter__();
+
+    def __getitem__(self, index):
+        return self._values[index]
+
+    def __len__(self):
+        return len(self._values)
+
+    def __repr__(self):
+        return "Vector({})".format(str(self._values))
+
+    def __str__(self):
+        return "({})".format(", ".join(str(e) for e in self._values))
