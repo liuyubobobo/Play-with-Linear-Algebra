cd99f4e73f8c06bb1cf1684d68a647b4d60202ee
liuyubobobo <liuyubobobo@gmail.com>
2018-07-26 01:50:03 -0700

    Chapter 03 completed.

--- 03-More-about-Vectors/06-Using-Numpy/main_numpy_vector.py
+++ 03-More-about-Vectors/06-Using-Numpy/main_numpy_vector.py
@@ -2,6 +2,8 @@ import numpy as np
 
 if __name__ == "__main__":
 
+    print(np.__version__)
+
     # np.array 基础
     lst = [1, 2, 3]
     lst[0] = "Linear Algebra"
@@ -10,28 +12,24 @@ if __name__ == "__main__":
     vec = np.array([1, 2, 3])
     print(vec)
     # vec[0] = "Linear Algebra"
+    # vec[0] = 666
+    # print(vec)
 
     # np.array的创建
     print(np.zeros(5))
     print(np.ones(5))
     print(np.full(5, 666))
-    print(np.arange(0, 20, 2))
-    print(np.arange(0, 10))
-    print(np.arange(10))
-    print(np.arange(0, 1, 0.2))
-    print(np.linspace(0, 1, 10))
-    print(np.linspace(0, 1, 11))
 
-    #np.array的基本属性
+    # np.array的基本属性
     print(vec)
-    print("ndim =", vec.ndim)
     print("size =", vec.size)
     print("size =", len(vec))
     print(vec[0])
     print(vec[-1])
-    print(vec[0:2])
+    print(vec[0: 2])
+    print(type(vec[0: 2]))
 
-    #np.array的基本运算
+    # np.array的基本运算
     vec2 = np.array([4, 5, 6])
     print("{} + {} = {}".format(vec, vec2, vec + vec2))
     print("{} - {} = {}".format(vec, vec2, vec - vec2))
@@ -41,6 +39,7 @@ if __name__ == "__main__":
 
     print(np.linalg.norm(vec))
     print(vec / np.linalg.norm(vec))
+    print(np.linalg.norm(vec / np.linalg.norm(vec)))
 
-    zero3 = np.zeros(3)
-    print(zero3 / np.linalg.norm(zero3))
+    # zero3 = np.zeros(3)
+    # print(zero3 / np.linalg.norm(zero3))
