abae81e22bb53714e4cc66760e74155dad1e5fe3
liuyubobobo <liuyubobobo@gmail.com>
2018-08-07 00:25:10 -0700

    Chapter 05 section 03 completed.

--- 05-More-about-Matrix/03-Implement-Matrix-Transformations/main_matrix_transformation.py
+++ 05-More-about-Matrix/03-Implement-Matrix-Transformations/main_matrix_transformation.py
@@ -6,10 +6,12 @@ import math
 
 if __name__ == "__main__":
 
-    points = [[0, 0], [0, 5], [3, 5], [3, 4], [1, 4], [1, 3], [2, 3], [2, 2], [1, 2], [1, 0]]
+    points = [[0, 0], [0, 5], [3, 5], [3, 4], [1, 4],
+              [1, 3], [2, 3], [2, 2], [1, 2], [1, 0]]
     x = [point[0] for point in points]
     y = [point[1] for point in points]
 
+    plt.figure(figsize=(5, 5))
     plt.xlim(-10, 10)
     plt.ylim(-10, 10)
     plt.plot(x, y)
