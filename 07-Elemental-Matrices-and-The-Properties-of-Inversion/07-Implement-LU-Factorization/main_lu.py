18abfeceec72d67939431aa429b227824d2baf08
liuyubobobo <liuyubobobo@gmail.com>
2018-09-21 13:42:51 -0700

    Chapter 07 structure updated.

--- 07-Elemental-Matrices-and-The-Properties-of-Inversion/07-Implement-LU-Factorization/main_lu.py
+++ 07-Elemental-Matrices-and-The-Properties-of-Inversion/07-Implement-LU-Factorization/main_lu.py
@@ -3,10 +3,26 @@ from playLA.LU import lu
 
 
 if __name__ == "__main__":
+    A1 = Matrix([[1, 2, 3], [4, 5, 6], [3, -3, 5]])
+    L1, U1 = lu(A1)
+    print(L1)
+    print(U1)
+    print(L1.dot(U1))
 
-    A = Matrix([[1, 2, 3], [4, 5, 6], [3, -3, 5]])
-    L, U = lu(A)
-    print(L)
-    print(U)
+    print()
 
-    print(L.dot(U))
+    A2 = Matrix([[1, 4, 5, 3], [5, 22, 27, 11], [6, 19, 27, 31], [5, 28, 35, -8]])
+    L2, U2 = lu(A2)
+    print(L2)
+    print(U2)
+    print(L2.dot(U2))
+
+    print()
+
+    A3 = Matrix([[1, 2, 3], [3, 7, 14], [4, 13, 38]])
+    L3, U3 = lu(A3)
+    print(L3)
+    print(U3)
+    print(L3.dot(U3))
+
+    print()
